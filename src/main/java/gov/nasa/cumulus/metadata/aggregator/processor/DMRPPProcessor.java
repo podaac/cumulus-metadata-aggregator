bdc4e3800bc3ce8559d8c39891ffb990fbff17d1
yenes56 <eshin_yen@hotmail.com>
2022-08-29 16:24:42 -0700

    removed OPeNDAP related URL objects (#12)
    Co-authored-by: Yen, David (398B-Affiliate) <david.yen@jpl.nasa.gov>

--- src/main/java/gov/nasa/cumulus/metadata/aggregator/processor/DMRPPProcessor.java
+++ src/main/java/gov/nasa/cumulus/metadata/aggregator/processor/DMRPPProcessor.java
@@ -1,18 +1,21 @@
 package gov.nasa.cumulus.metadata.aggregator.processor;
 
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import com.vividsolutions.jts.io.ParseException;
 import cumulus_message_adapter.message_parser.AdapterLogger;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Paths;
+import java.util.Iterator;
 
 public class DMRPPProcessor extends ProcessorBase{
     private final String className = this.getClass().getName();
     public String process(String input, String ummgStr, String region, String revisionId)
             throws IOException, ParseException {
         try {
+            ummgStr = cleanseOPeNDAPUrl(ummgStr);
             String cmrBucket = System.getenv().getOrDefault("INTERNAL_BUCKET", "");
             String cmrDir = System.getenv().getOrDefault("CMR_DIR", "");
             AdapterLogger.LogDebug(this.className + " internal bucket: " + cmrBucket + " CMR Dir: " + cmrDir);
@@ -33,6 +36,27 @@ public class DMRPPProcessor extends ProcessorBase{
             AdapterLogger.LogError("Footprint processor exception:" + e);
             throw e;
         }
+    }
+
+    private String cleanseOPeNDAPUrl(String ummgStr) {
+        Gson gsonBuilder = getGsonBuilder();
+        JsonObject cmrJsonObj = JsonParser.parseString(ummgStr).getAsJsonObject();
+        JsonArray relatedUrls = cmrJsonObj.getAsJsonArray("RelatedUrls");
+        if (relatedUrls == null) {
+            return ummgStr;
+        }
+
+        Iterator<JsonElement> iterator = relatedUrls.iterator();
+        while(iterator.hasNext()){
+            JsonElement node = iterator.next();
+            JsonElement subTypeElement = node.getAsJsonObject().get("Subtype");
+            if (subTypeElement != null && StringUtils.equalsIgnoreCase(
+                    StringUtils.trim(subTypeElement.getAsString()), "OPENDAP DATA")) {
+                AdapterLogger.LogInfo(this.className + " found OPENDAP LINK and removing: " + node);
+                iterator.remove();
+            }
+        }
+        return gsonBuilder.toJson(cmrJsonObj);
     }
 
 }
